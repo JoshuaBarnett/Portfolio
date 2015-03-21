b411efab76ea183d370baf2a7aaacaec96e870b3
JoshuaBarnett <jbarnett@knights.ucf.edu>
2015-03-20 21:01:02 -0400

    Create salesdata.erl

---
+++ Erlang/HomeworkAssignments/salesdata.erl
@@ -0,0 +1,8 @@
+%Support File necessary for substaddr.erl, written by Gary Leavens
+
+% $Id: salesdata.erl,v 1.4 2015/03/12 12:15:20 leavens Exp $
+-module(salesdata).
+-include("salesdata.hrl").
+-export_type([salesdata/0]).
+
+-type salesdata() :: #store{} | #group{}.
